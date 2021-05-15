b02defd49ef4765a8869be02d214a9bdeca53cd6
FangYuan <2056079142@qq.com>
2021-05-15 20:31:35 +0800

    first time

---
+++ src/views/List.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="home">
+        <!--轮播图-->
+<!--        <swiper/>-->
+        <!--热门歌单/歌手-->
+<!--        <div class="section" v-for="(item, index) in songsList" :key="index">-->
+<!--            <div class="section-title">{{item.name}}</div>-->
+<!--&lt;!&ndash;            <content-list :contentList="item.list">1234</content-list>&ndash;&gt;-->
+<!--            <div class="section">1234</div>-->
+<!--        </div>-->
+        <div class="section" v-for="(item, index) in songsList" :key="index">
+            <div class="section-title">{{item.name}}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    // import Swiper from '../components/Swiper'
+    // import ContentList from '../components/ContentList'
+    // import { getSongList, getAllSinger } from '../api/index'
+
+    export default {
+        name: 'home',
+        // components: {
+        //     Swiper,
+        //     ContentList
+        // },
+        data () {
+            return {
+                songsList: [
+                    {name: '歌单', list: []},
+                    {name: '歌手', list: []}
+                ]
+            }
+        },
+        created () {
+            // // 获取歌单列表
+            // this.getSongList('songList')
+            // // 获取歌手列表
+            // this.getSinger('singer')
+        },
+        methods: {
+            // getSongList (path) {
+            //     getSongList()
+            //         .then(res => {
+            //             this.songsList[0].list = res.slice(0, 10)
+            //         })
+            //         .catch(err => {
+            //             console.log(err)
+            //         })
+            // },
+            // getSinger () {
+            //     getAllSinger().then(res => {
+            //         this.songsList[1].list = res.slice(0, 10)
+            //     })
+            //         .catch(err => {
+            //             console.log(err)
+            //         })
+            // }
+        }
+    }
+</script>
+
+<!--<style lang="scss" scoped>-->
+<!--    @import '../assets/css/home.scss';-->
+<!--</style>-->
