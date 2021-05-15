c5dda212e03eecc6acb1e0ab0cdde128aebba95f
FangYuan <2056079142@qq.com>
2021-05-15 20:55:12 +0800

    first commit

--- src/views/List.vue
+++ src/views/List.vue
@@ -16,15 +16,15 @@
 
 <script>
     // import Swiper from '../components/Swiper'
-    // import ContentList from '../components/ContentList'
-    // import { getSongList, getAllSinger } from '../api/index'
+     import ContentList from '../components/ContentList'
+     import { getSongList, getAllSinger } from '../api/index'
 
     export default {
         name: 'home',
-        // components: {
+         components: {
         //     Swiper,
-        //     ContentList
-        // },
+             ContentList
+         },
         data () {
             return {
                 songsList: [
@@ -34,33 +34,33 @@
             }
         },
         created () {
-            // // 获取歌单列表
-            // this.getSongList('songList')
-            // // 获取歌手列表
-            // this.getSinger('singer')
+            // 获取歌单列表
+            this.getSongList('songList')
+            // 获取歌手列表
+            this.getSinger('singer')
         },
         methods: {
-            // getSongList (path) {
-            //     getSongList()
-            //         .then(res => {
-            //             this.songsList[0].list = res.slice(0, 10)
-            //         })
-            //         .catch(err => {
-            //             console.log(err)
-            //         })
-            // },
-            // getSinger () {
-            //     getAllSinger().then(res => {
-            //         this.songsList[1].list = res.slice(0, 10)
-            //     })
-            //         .catch(err => {
-            //             console.log(err)
-            //         })
-            // }
+            getSongList (path) {
+                getSongList()
+                    .then(res => {
+                        this.songsList[0].list = res.slice(0, 10)
+                    })
+                    .catch(err => {
+                        console.log(err)
+                    })
+            },
+            getSinger () {
+                getAllSinger().then(res => {
+                    this.songsList[1].list = res.slice(0, 10)
+                })
+                    .catch(err => {
+                        console.log(err)
+                    })
+            }
         }
     }
 </script>
 
-<!--<style lang="scss" scoped>-->
-<!--    @import '../assets/css/home.scss';-->
-<!--</style>-->
+<style lang="scss" scoped>
+    @import '../assets/css/home.scss';
+</style>
